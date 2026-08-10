f3878183a3a844c9f7606d2d108b057572a55a72
Cavz <ornb.developer@gmail.com>
2026-08-09 22:42:15 -0300

    feat: cria estrutura inicial do CRM

---
+++ js/app.js
@@ -0,0 +1,18 @@
+const sidebarToggle = document.querySelector("#sidebar-toggle");
+sidebarToggle.addEventListener("click", function () {
+    const sidebarCollapsed = document.body.classList.toggle(
+        "sidebar-collapsed"
+    );
+
+    sidebarToggle.setAttribute(
+        "aria-expanded",
+        String(!sidebarCollapsed)
+    );
+
+    sidebarToggle.setAttribute(
+        "aria-label",
+        sidebarCollapsed
+            ? "Expandir menu"
+            : "Recolher menu"
+    );
+});
